799086dfc1d34144a41a8dc62b321d3fcdb91755
Irsad99 <irsadmoh01@gmail.com>
2022-04-12 09:33:34 +0700

    modified code fetch Rest API

--- week2_intro3/latihanRestAPI.js
+++ week2_intro3/latihanRestAPI.js
@@ -1,9 +1,17 @@
-// const getPost = () => fetch('https://jsonplaceholder.typicode.com/users')
-// .then((result) => {
-//     return result.json()
-// })
-// .then((user) => {
-//     console.log(user);
-// });
+const fetchPromise = fetch('https://jsonplaceholder.typicode.com/users')
 
-// getPost()
+fetchPromise
+  .then( response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then( json => {
+    for (let index = 0; index < json.length; index++) {
+        console.log(json[index].name);
+    }
+  })
+  .catch( error => {
+    console.error(`Could not get products: ${error}`);
+  });
