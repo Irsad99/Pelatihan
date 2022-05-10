07dbcbec2d400bd9d3874e18d3c2604aa080e150
Irsad99 <irsadmoh01@gmail.com>
2022-05-10 12:09:29 +0700

    add cors

--- backEndTickitz/app.js
+++ backEndTickitz/app.js
@@ -8,6 +8,17 @@ const cors = require('cors');
 const server = express();
 const PORT = 8080;
 
+// const whitelist = ['http://localhost:8080/', 'http://localhost:3000/']
+// const corsOptions = {
+//     origin: function (origin, callback) {
+//         if (whitelist.indexOf(origin) !== -1) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     }
+// }
+
 server.use(express.urlencoded({ extended: false }));
 server.use(express.json());
 server.use(cors());
